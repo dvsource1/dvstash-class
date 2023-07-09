c68fe90ccfe3ed965a0f7d74396c7c02b952bcb3
Viraj Kaushalye <virajkaush@gmail.com>
2023-07-10 00:03:29 +0530

    clerk auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+import { SignIn, SignUp, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="">
-      <h1>DV Stash Class-Room</h1>
+    <main className="container mx-auto pt-6">
+      <header>
+        <h1 className="text-4xl">DV Stash Class-Room</h1>
+        <p className="text-stone-500">
+          Excel in your O/L Science exam with our interactive practice platform.
+          Access a vast question bank, track progress and boost your confidence.
+        </p>
+      </header>
+      <div className="mx-auto flex w-full pt-4">
+        <SignIn />
+        <UserButton />
+      </div>
     </main>
   )
 }
