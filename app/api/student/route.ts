72d24b789c4d7d046338c6d899ab0afe768438d8
Viraj Kaushalye <virajkaush@gmail.com>
2023-07-10 01:45:36 +0530

    redis setup

--- app/api/student/route.ts
+++ app/api/student/route.ts
@@ -1,6 +1,43 @@
 import prismadb from '@/lib/prismadb'
+import redisdb from '@/lib/redisdb'
 import { auth } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
+
+export async function GET(req: Request) {
+  try {
+    const { searchParams } = new URL(req.url)
+    const id = searchParams.get('id')
+
+    // if (!userId) {
+    //   return new NextResponse('Unauthorized', { status: 401 })
+    // }
+
+    if (!id) {
+      return new NextResponse('User ID is required', { status: 400 })
+    }
+
+    const redisStudent = await redisdb.get(`STUDENT.${id}`)
+
+    if (redisStudent) {
+      return NextResponse.json(redisStudent)
+    }
+
+    const prismaStudent = await prismadb.student.findFirst({
+      where: { id },
+    })
+
+    if (!prismaStudent) {
+      return new NextResponse('User not found', { status: 400 })
+    }
+
+    redisdb.set(`STUDENT.${prismaStudent.id}`, prismaStudent)
+    console.log('[API/STUDENT | GET]', prismaStudent)
+    return NextResponse.json(prismaStudent)
+  } catch (err) {
+    console.error('[API/STUDENT | GET]', err)
+    return new NextResponse('Internal Server Error', { status: 500 })
+  }
+}
 
 export async function POST(req: Request) {
   try {
@@ -33,6 +70,8 @@ export async function POST(req: Request) {
         year,
       },
     })
+
+    redisdb.set(`STUDENT.${student.id}`, student)
 
     return NextResponse.json(student)
   } catch (err) {
---
+++ lib/redisdb.ts
@@ -0,0 +1,15 @@
+import { Redis } from '@upstash/redis'
+
+declare global {
+  var redis: Redis | undefined
+}
+
+const redisdb =
+  globalThis.redis ||
+  new Redis({
+    url: process.env.UPSTASH_REDIS_REST_URL,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  })
+if (process.env.NODE_ENV === 'production') globalThis.redis = redisdb
+
+export default redisdb
